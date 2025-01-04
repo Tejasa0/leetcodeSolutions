bab82f874ec6a89482e934432744288811ed7a3a
Tejas Patil <tejaspatila0@gmail.com>
2025-01-04 19:59:12 +0530

    Time: 1034 ms (14.4%), Space: 47.3 MB (12.83%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -6,18 +6,21 @@ class Solution {
         HashMap<Character,Integer>hm=new HashMap<>();
         for(int i=0;i<len;i++)
         {
+            if(!hm.containsKey(s.charAt(i)))
+            hm.put(s.charAt(i),i);
+        }
+        int arr[]=new int[26];
+        for(int i=len-1;i>=2;i--)
+        {
             char curr=s.charAt(i);
-            if(hm.containsKey(curr))
+            if(arr[s.charAt(i)-'a']==0 && hm.containsKey(curr))
             {
                 for(int j=hm.get(curr)+1;j<i;j++)
                 {
                   uniquePal.add(""+curr+s.charAt(j)+curr);  
                 }
+                arr[s.charAt(i)-'a']=1;
             }
-            else
-            hm.put(curr,i);
-            if(uniquePal.size()==676)
-            break;
         }
         return uniquePal.size();
     }
