b080fb93e58bbbffe57efdc9e1dd108ffaa86175
Tejas Patil <tejaspatila0@gmail.com>
2025-01-12 22:54:34 +0530

    Time: 1 ms (99.75%), Space: 41.9 MB (85.15%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum=0;
+        int n=s.length();
+        for(int i=0;i<n-1;i++)
+        {
+            sum+=Math.abs((s.charAt(i)-'a')-(s.charAt(i+1)-'a'));
+        }
+        return sum;
+    }
+}
