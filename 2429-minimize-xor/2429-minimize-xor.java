cc7a89d00d4379f1c439ab2f98c30f69c20cd661
Tejas Patil <tejaspatila0@gmail.com>
2025-01-15 21:12:16 +0530

    Time: 0 ms (100%), Space: 40.6 MB (46.23%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.java
@@ -0,0 +1,30 @@
+public class Solution {
+
+    public int minimizeXor(int num1, int num2) {
+        int result = 0;
+
+        int targetSetBitsCount = Integer.bitCount(num2);
+        int setBitsCount = 0;
+        int currentBit = 31; 
+
+        while (setBitsCount < targetSetBitsCount) {
+
+            if (
+                isSet(num1, currentBit) ||
+                (targetSetBitsCount - setBitsCount > currentBit)
+            ) {
+                result = setBit(result, currentBit);
+                setBitsCount++;
+            }
+            currentBit--;
+        }
+
+        return result;
+    }
+    private boolean isSet(int x, int bit) {
+        return (x & (1 << bit)) != 0;
+    }
+    private int setBit(int x, int bit) {
+        return x | (1 << bit);
+    }
+}
