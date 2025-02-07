1f54253ae9976a0d058ffb2aee347f89e36fa854
Tejas Patil <tejaspatila0@gmail.com>
2025-02-07 19:33:05 +0530

    Time: 40 ms (72.05%), Space: 97.4 MB (52.84%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,30 +1,26 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        int arr[]=new int[limit+1];
-        int ans[]=new int[queries.length];
-        int count=0;
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<queries.length;i++)
+        HashMap<Integer,Integer>countHm=new HashMap<>();
+        HashMap<Integer,Integer>idxHm=new HashMap<>();
+        int len=queries.length;
+        int ans[]=new int[len];
+        int i=0;
+        for(int[] query:queries)
         {
-            int idx=queries[i][0];
-            int col=queries[i][1];
-            if(arr[idx]==0 && !(hm.containsKey(col) && hm.get(col)!=0))
+            int idx=query[0];
+            int col=query[1];
+        
+            if(idxHm.containsKey(idx))
             {
-                count++;
+                int existCol=idxHm.get(idx);
+                countHm.put(existCol,countHm.get(existCol)-1);
+                if(countHm.get(existCol)==0)
+                countHm.remove(existCol);
             }
-            else if(arr[idx]!=0 && hm.containsKey(col) && hm.get(col)!=0 && arr[idx]!=col)
-            count--;
-            else if(arr[idx]!=0 && hm.containsKey(col) && hm.get(col)==0 && arr[idx]!=col)
-            count++;
-            int cur=arr[idx];
-            if(hm.containsKey(cur))
-            hm.put(cur,hm.get(cur)-1);
-            arr[idx]=col;
-            ans[i]=count;
-            if(hm.containsKey(col))
-            hm.put(col,hm.get(col)+1);
-            else
-            hm.put(col,1);
+            idxHm.put(idx,col);
+            countHm.put(col,countHm.getOrDefault(col,0)+1);
+            ans[i++]=countHm.size();
+            
         }
         return ans;
     }
