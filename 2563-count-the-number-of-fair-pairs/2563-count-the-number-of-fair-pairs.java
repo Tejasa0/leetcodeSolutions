923efdc87399f30b7794ca51a1532e6985d15d91
Tejas Patil <tejaspatila0@gmail.com>
2025-04-20 12:14:33 +0530

    Time: 65 ms (34.41%), Space: 57.3 MB (56.95%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -5,34 +5,33 @@ class Solution {
         long count = 0;
 
         for (int i = 0; i < n; i++) {
-            int left = lowerBound(nums, i + 1, n - 1, lower - nums[i]);
-            int right = upperBound(nums, i + 1, n - 1, upper - nums[i]);
+            int left = lowerUpperBound(nums, i + 1, n - 1, lower - nums[i], true);
+            int right = lowerUpperBound(nums, i + 1, n - 1, upper - nums[i], false);
             count += (right - left + 1);
         }
 
         return count;
     }
-    private int lowerBound(int[] nums, int start, int end, int target) {
-        int l = start, r = end;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-        return l;
-    }
 
-    private int upperBound(int[] nums, int start, int end, int target) {
-        int l = start, r = end;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] <= target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+    private int lowerUpperBound(int[] nums, int start, int end, int target, boolean lowerUpperFlag) {
+        if (lowerUpperFlag) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return start;
+        } else {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] <= target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return end;
         }
-        return r;
     }
 }
