9311c3a1d01c90b5df01abd317c9765dfbc92aee
Tejas Patil <tejaspatila0@gmail.com>
2025-05-19 21:31:38 +0530

    Time: 1 ms (34.56%), Space: 42 MB (60.66%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.java
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -1,20 +1,14 @@
 class Solution {
     public String triangleType(int[] nums) {
-        int c = Math.max(nums[0], Math.max(nums[1], nums[2]));
-        int a = Math.min(nums[0], Math.min(nums[1], nums[2]));
-        int b;
-        if (nums[0] != a && nums[0] != c)
-            b = nums[0];
-        else if (nums[1] != c && nums[1] != a)
-            b = nums[1];
-        else
-            b = nums[2];
-        if (a + b <= c)
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) {
             return "none";
-        if (a == b && b == c)
+        } else if (nums[0] == nums[2]) {
             return "equilateral";
-        else if (a == b || a == c || b == c)
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
             return "isosceles";
-        return "scalene";
+        } else {
+            return "scalene";
+        }
     }
 }
