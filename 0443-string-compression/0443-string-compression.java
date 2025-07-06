dff0d583925743a1901e22fc95a2aef2ccf3c8fc
Tejas Patil <tejaspatila0@gmail.com>
2025-07-06 11:38:33 +0530

    Time: 1 ms (99.42%), Space: 44.6 MB (10.86%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,50 +1,34 @@
 class Solution {
-public static int compress(char[] chars) {
-        int n=1;
-        int total=0;
-        int j=0;
-        if(chars.length<=1)
-        {
-            return chars.length;
-        }
-        for(int i=0;i<chars.length-1;i++)
-        {
-            if(chars[i]==chars[i+1])
-            {
-                n++;
+    public int compress(char[] chars) {
+        int j = 0;
+        int count = 1;
+        int i = 0;
+        for (i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                count++;
+            } else {
+                chars[j++] = chars[i];
+                if (count > 9) {
+                    String s = Integer.toString(count);
+                    for (char c : s.toCharArray()) {
+                        chars[j++] = c;
+                    }
+                } else if (count > 1) {
+                    chars[j++] = (char) (count + '0');
+                }
+                count = 1;
             }
-            else
-            {
-                chars[j++]=chars[i];
-                if(n>1){
-                String a=Integer.toString(n);
-                
-                for(int k=0;k<a.length();k++)
-                {
-                    chars[j++]=a.charAt(k);
-                }
-                n=1;
-                total+=a.length();
-                }
-                total+=1;
-                
-                
-            }
-            
-        }
-        chars[j++]=chars[chars.length-1];
-        if(n>1){
-                String a=Integer.toString(n);
-                
-                for(int k=0;k<a.length();k++)
-                {
-                    chars[j++]=a.charAt(k);
-                }
-                n=1;
-                total+=a.length();
-                }
 
-                total+=1;
-        return total;
+        }
+        chars[j++] = chars[i];
+        if (count > 9) {
+            String s = Integer.toString(count);
+            for (char c : s.toCharArray()) {
+                chars[j++] = c;
+            }
+        } else if (count > 1) {
+            chars[j++] = (char) (count + '0');
+        }
+        return j;
     }
 }
