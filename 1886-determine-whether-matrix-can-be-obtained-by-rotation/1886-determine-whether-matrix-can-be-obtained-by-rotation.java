26a5200addd9a12ccb9299081646763458ed7f5e
Tejas Patil <tejaspatila0@gmail.com>
2026-03-22 20:12:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
         for (int i = 0; i < 4; i++) {
-            if (isEqual(mat, target))
+            if (isEqual(mat, target, n))
                 return true;
-            mat = rotate(mat);
+            mat = rotate(mat, n);
         }
         return false;
     }
 
-    private int[][] rotate(int[][] mat) {
-        int n = mat.length;
+    private int[][] rotate(int[][] mat, int n) {
         int[][] rotated = new int[n][n];
 
         for (int i = 0; i < n; i++) {
@@ -20,8 +20,7 @@ class Solution {
         return rotated;
     }
 
-    private boolean isEqual(int[][] a, int[][] b) {
-        int n = a.length;
+    private boolean isEqual(int[][] a, int[][] b, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (a[i][j] != b[i][j])
