ff74350ee23cb0fdb2c0f0cf9827463fc7ea128e
Tejas Patil <tejaspatila0@gmail.com>
2026-05-28 12:00:08 +0530

    Time: 7 ms (100%), Space: 48 MB (80.16%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,32 +1,30 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int [] count=new int[26];
-        int [] count1=new int[26];
-        for(char c : word.toCharArray()){
+        int[] count = new int[26];
+        int[] count1 = new int[26];
+        int j = 1;
+        for (char c : word.toCharArray()) {
             int s = c - 'a';
             int b = c - 'A';
-            if(s>-1 && s<26){
-                if(count1[s]!=0){
-                    count1[s]=-1;
-                    continue;
-                }
-                count[s]++;
+            if (s > -1 && s < 26) {
+
+                count[s] = j;
+
             }
-            if(b>-1 && b<26){
-                if(count[b]!=0 && count1[b]!=-1)
-                count1[b]++;
-                else
-                count1[b]=-1;
+            if (b > -1 && b < 26) {
+                if (count1[b] == 0)
+                    count1[b] = j;
+
             }
+            j++;
         }
-        int ans=0;
-        for(int i=0;i<26;i++){
-            System.out.println(count1[i]);
-            if(count[i]>0 && count1[i]>0){
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i]!= 0 && count[i] < count1[i]) {
                 ans++;
             }
         }
         return ans;
-        
+
     }
 }
