8150a4593722e0a895214aa4fc1849788e1081b3
Tejas Patil <tejaspatila0@gmail.com>
2026-06-27 19:51:00 +0530

    Time: 31 ms (83.29%), Space: 59.1 MB (59.54%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>>li=new ArrayList<>();
-        for(int i=0;i<nums.length-2;i++)
-        {
-            if(i>0 && nums[i]==nums[i-1])
-            continue;
-            int firstIndex=i+1;
-            int secondIndex=nums.length-1;
-            while(firstIndex<secondIndex)
-            {
-                int sum=nums[firstIndex]+nums[secondIndex]+nums[i];
-                if(sum==0)
-                {
-                    li.add(Arrays.asList(nums[i],nums[firstIndex],nums[secondIndex]));
-                    while(firstIndex<secondIndex && nums[firstIndex]==nums[firstIndex+1])
-                        firstIndex++;
-                    firstIndex++;
+        List<List<Integer>> li = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (i > 0 && i < nums.length && nums[i] == nums[i - 1]) {
+                i++;
+            }
+            int sec = i + 1;
+            int third = nums.length - 1;
+            while (sec < third) {
+                int sum = nums[i] + nums[sec] + nums[third];
+                if (sum < 0) {
+                    sec++;
+                } else if (sum > 0) {
+                    third--;
+                } else {
+                    li.add(Arrays.asList(nums[i], nums[sec], nums[third]));
+                    sec++;
+                    while (sec < third && nums[sec] == nums[sec - 1]) {
+                        sec++;
+                    }
+                    while (sec < third && nums[third] == nums[third - 1]) {
+                        third--;
+                    }
+
+                    third--;
                 }
-                else if(sum<0)
-                firstIndex++;
-                else
-                secondIndex--;
             }
         }
         return li;
