b2fda8ceb4289351b3c813625091744d8a6e63b3
Tejas Patil <tejaspatila0@gmail.com>
2026-06-27 19:47:14 +0530

    Time: 32 ms (78.96%), Space: 59.3 MB (32.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,34 +2,26 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>>li=new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<nums.length-2;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
+            continue;
+            int firstIndex=i+1;
+            int secondIndex=nums.length-1;
+            while(firstIndex<secondIndex)
             {
-                continue;
-            }
-            int sec=i+1;
-            int third=nums.length-1;
-            while(sec<third)
-            {
-                int sum=nums[i]+nums[sec]+nums[third];
-                if(sum<0)
-                {    
-                    sec++;
-                }
-                else if(sum>0)
+                int sum=nums[firstIndex]+nums[secondIndex]+nums[i];
+                if(sum==0)
                 {
-                    third--;
+                    li.add(Arrays.asList(nums[i],nums[firstIndex],nums[secondIndex]));
+                    while(firstIndex<secondIndex && nums[firstIndex]==nums[firstIndex+1])
+                        firstIndex++;
+                    firstIndex++;
                 }
+                else if(sum<0)
+                firstIndex++;
                 else
-                {
-                    li.add(Arrays.asList(nums[i],nums[sec],nums[third]));
-                    sec++;
-                    while(sec<third && nums[sec]==nums[sec-1])
-                    {
-                        sec++;
-                    }
-                }
+                secondIndex--;
             }
         }
         return li;
